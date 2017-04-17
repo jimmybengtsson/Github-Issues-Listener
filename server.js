bdfd3b675db12600ef1d9e0bfbf98bee53df76a8
jimmybengtsson <jb223pu@student.lnu.se>
2017-04-17 18:39:25 +0200

    Push to production

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ io.on('connection', function(socket) {
 
 webhookHandler.on('*', function (event, repo, data) {
 
-    console.log(event + bodyParser.json(data) + repo);
+    console.log(event + data.issue.title + repo);
 
 });
 
