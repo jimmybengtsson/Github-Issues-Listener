0bf916a7f9cf7a21f9ba6680f786e5e6e521f810
jimmybengtsson <jb223pu@student.lnu.se>
2017-04-18 22:14:56 +0200

    Push to production

--- public/js/Socket.js
+++ public/js/Socket.js
@@ -1,18 +1,16 @@
+"use strict";
+
 let socket = io();
 
 socket.on('allIssues', function(issues) {
 
     console.log('All issue on');
 
-    /*for (let i in issues) {
-        ulList.appendChild(createNewIssueDiv(issues[i]));
-        console.log(ulList);
+    issues.forEach((i) => {
 
-    }*/
-    //issues.forEach((i) => {
+        getIssues(i);
 
-    console.log(issues);
-    //});
+    });
 });
 
 socket.on('newIssue', function(data) {
@@ -55,4 +53,37 @@ function createNewIssueDiv(issue) {
 
     ulList.insertBefore(issueLi, ulList.childNodes[0]);
 }
+
+function getIssues(issue) {
+
+    let ulList = document.querySelector('.ulClass');
+
+    let template = document.querySelector('.issueTemplate');
+    let clone = document.importNode(template.content, true);
+    let issueLi = clone.querySelector('.issueLi');
+    let issueDiv = clone.querySelector('.issueDiv');
+    let author = clone.querySelector('.issueAuthor');
+    let title = clone.querySelector('.issueTitle');
+    let text = clone.querySelector('.issueText');
+    let img = clone.querySelector('.issueImg');
+    let issueLink = clone.querySelector('.issueLink');
+
+    author.textContent = 'Created by: ' + issue.user.login;
+    author.style.color = '#FF0208';
+    title.textContent = 'Title: ' + issue.title;
+    text.textContent = 'Message: ' + issue.body;
+    img.src = issue.user.avatar_url;
+    issueLink.setAttribute('href', issue.html_url);
+
+    issueDiv.appendChild(img);
+    issueDiv.appendChild(author);
+
+    issueLi.appendChild(issueDiv);
+    issueLi.appendChild(title);
+    issueLi.appendChild(text);
+    issueLi.appendChild(issueLink);
+
+    ulList.insertBefore(issueLi, ulList.childNodes[0]);
+}
+
 
