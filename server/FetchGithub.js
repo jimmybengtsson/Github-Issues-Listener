db1820155d35cf50946a843572f11f644eec5b2e
jimmybengtsson <jb223pu@student.lnu.se>
2017-04-18 20:56:07 +0200

    Push to production

--- server/FetchGithub.js
+++ server/FetchGithub.js
@@ -6,21 +6,19 @@ function FetchGithub() {
 
     let options = {
         uri: process.env.ISSUES_URL,
-        method: 'GET',
         qs: {access_token: process.env.GITHUB_KEY},
-        headers: {'User-Agent': 'Request-Promise'},
+        headers: {'User-Agent': process.env.APP_NAME},
         json: true
-
     };
 
     return rp(options)
         .then(function(data) {
 
-            console.log('then    ' + data);
+            console.log(data);
             return data;
 
     }).catch(function(err) {
-        console.log('catch    ' + err);
+        console.log(err);
     });
 
 }
