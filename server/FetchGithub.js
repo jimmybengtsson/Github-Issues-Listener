735f83b919a3bbead05b285a7b236e9bee9f062c
jimmybengtsson <jb223pu@student.lnu.se>
2017-04-18 21:52:42 +0200

    Push to production

--- server/FetchGithub.js
+++ server/FetchGithub.js
@@ -5,8 +5,7 @@ let rp = require('request-promise');
 function FetchGithub() {
 
     let options = {
-        uri: 'https://api.github.com',
-        path: process.env.ISSUES_PATH,
+        uri: process.env.ISSUES_URL,
         qs: {access_token: process.env.GITHUB_KEY},
         headers: {'user-agent':'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)'},
         json: true
