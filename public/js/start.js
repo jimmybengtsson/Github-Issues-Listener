3805fcbf18d13a6e4e03177c2f4767b693e4d83e
jimmybengtsson <jb223pu@student.lnu.se>
2017-04-19 16:43:23 +0200

    Push to production

--- public/js/start.js
+++ public/js/start.js
@@ -70,3 +70,17 @@ window.addEventListener('load', function() {
 
     Clock();
 });
+
+// Service worker
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/js/sw.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
